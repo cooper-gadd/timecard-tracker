e4626c4d3f31fe12c799dc0a8043eeace7fd235d
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-08 22:01:22 -0500

    delete company

--- src/main/java/com/project/two/MyResource.java
+++ src/main/java/com/project/two/MyResource.java
@@ -1,9 +1,15 @@
 package com.project.two;
 
+import com.project.two.business.BusinessLayer;
+import jakarta.json.Json;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -21,5 +27,23 @@ public class MyResource {
   @Produces(MediaType.TEXT_PLAIN)
   public String getIt() {
     return "Got it!";
+  }
+
+  @DELETE
+  @Path("company/{companyName}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response deleteCompany(@PathParam("companyName") String companyName) {
+    BusinessLayer bl = new BusinessLayer();
+    JsonObjectBuilder job = Json.createObjectBuilder();
+    try {
+      bl.deleteCompany(companyName);
+      job.add("success", true);
+      return Response.status(Response.Status.OK).entity(job.build()).build();
+    } catch (Exception e) {
+      job.add("error", e.getMessage());
+      return Response.status(Response.Status.BAD_REQUEST)
+        .entity(job.build())
+        .build();
+    }
   }
 }
--- src/main/java/com/project/two/business/BusinessLayer.java
+++ src/main/java/com/project/two/business/BusinessLayer.java
@@ -1,3 +1,20 @@
 package com.project.two.business;
 
-public class BusinessLayer {}
+import companydata.DataLayer;
+
+public class BusinessLayer {
+
+  private DataLayer dl = null;
+
+  public BusinessLayer() {
+    dl = new DataLayer("ctg7866");
+  }
+
+  public void deleteCompany(String companyName) {
+    try {
+      dl.deleteCompany(companyName);
+    } catch (Exception e) {
+      System.out.println("Error in deleteCompany: " + e.getMessage());
+    }
+  }
+}
