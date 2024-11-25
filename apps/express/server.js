8cd47e38046b72f0f2d3c7e6742e1fb097b9da39
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-25 13:44:15 -0500

    delete company, t/c for depts

--- apps/express/server.js
+++ apps/express/server.js
@@ -12,9 +12,26 @@ app.get("/", async function (_req, res) {
   res.json({ response: "this is the appropriate response" });
 });
 
+app.delete("/company", async function (req, res) {
+  try {
+    dl.deleteCompany(req.query.company);
+    res.status(200).send("Company data deleted successfully");
+  } catch (error) {
+    res.status(400).send({
+      error: error.message,
+    });
+  }
+});
+
 app.get("/departments", async function (req, res) {
-  const departments = await dl.getAllDepartment(req.query.company);
-  res.send(departments);
+  try {
+    const departments = await dl.getAllDepartment(req.query.company);
+    res.status(200).send(departments);
+  } catch (error) {
+    res.status(400).send({
+      error: error.message,
+    });
+  }
 });
 
 app.listen(8282);
