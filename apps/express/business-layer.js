fc4de527dc8d48bad7bc7f2da69cab1db7dd0deb
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-25 18:27:05 -0500

    update emp bz

--- apps/express/business-layer.js
+++ apps/express/business-layer.js
@@ -108,3 +108,61 @@ export async function insertEmployee(
     ),
   );
 }
+
+export async function updateEmployee(
+  company,
+  emp_id,
+  emp_name,
+  emp_no,
+  hire_date,
+  job,
+  salary,
+  dept_id,
+  mng_id,
+) {
+  // company – must be your RIT username
+  if (company !== "ctg7866") {
+    throw new Error("Company name must be your RIT username");
+  }
+
+  // dept_id must exist as a Department in your company
+  if (!(await dl.getDepartment(company, dept_id))) {
+    throw new Error("Department not found");
+  }
+
+  // mng_id must be the record id of an existing Employee in your company. Use 0 if the first employee or any other employee that doesn’t have a manager.
+  if (!(await dl.getEmployee(mng_id)) && mng_id !== 0) {
+    throw new Error("Manager not found");
+  }
+
+  // hire_date must be a valid date equal to the current date or earlier (e.g. current date or in the past)
+  if (new Date(hire_date) > new Date()) {
+    throw new Error("Hire date must be in the past");
+  }
+
+  // hire_date must be a Monday, Tuesday, Wednesday, Thursday or a Friday. It cannot be Saturday or Sunday.
+  if (
+    new Date(hire_date).getDay() === 0 ||
+    new Date(hire_date).getDay() === 6
+  ) {
+    throw new Error("Hire date must be a weekday");
+  }
+
+  // emp_id must be a valid record id in the database
+  if (!(await dl.getEmployee(emp_id))) {
+    throw new Error("Employee not found");
+  }
+
+  return await dl.updateEmployee(
+    new dl.Employee(
+      emp_name,
+      // emp_no must be unique among all companies, Suggestion: include company name as part of id
+      emp_no + "_" + company,
+      hire_date,
+      job,
+      salary,
+      dept_id,
+      mng_id,
+    ),
+  );
+}
--- apps/express/server.js
+++ apps/express/server.js
@@ -136,51 +136,16 @@ app.post(root + "/exployee", async function (req, res) {
 
 app.put(root + "/exployee", async function (req, res) {
   try {
-    // company – must be your RIT username
-    if (req.body.company !== "ctg7866") {
-      throw new Error("Company name must be your RIT username");
-    }
-
-    // dept_id must exist as a Department in your company
-    if (!(await dl.getDepartment(req.body.company, req.body.dept_id))) {
-      throw new Error("Department not found");
-    }
-
-    // mng_id must be the record id of an existing Employee in your company. Use 0 if the first employee or any other employee that doesn’t have a manager.
-    if (!(await dl.getEmployee(req.body.mng_id)) && req.body.mng_id !== 0) {
-      throw new Error("Manager not found");
-    }
-
-    // hire_date must be a valid date equal to the current date or earlier (e.g. current date or in the past)
-    if (new Date(req.body.hireDate) > new Date()) {
-      throw new Error("Hire date must be in the past");
-    }
-
-    // hire_date must be a Monday, Tuesday, Wednesday, Thursday or a Friday. It cannot be Saturday or Sunday.
-    if (
-      new Date(req.body.hireDate).getDay() === 0 ||
-      new Date(req.body.hireDate).getDay() === 6
-    ) {
-      throw new Error("Hire date must be a weekday");
-    }
-
-    // emp_id must be a valid record id in the database
-    if (!(await dl.getEmployee(req.body.emp_id))) {
-      throw new Error("Employee not found");
-    }
-
-    const employee = await dl.updateDepartment(
-      new dl.Employee(
-        req.body.emp_id,
-        req.body.emp_name,
-        // emp_no must be unique amongst all employees in the database, including those of other companies. You may wish to include your RIT user ID in the employee number somehow.
-        req.body.emp_no + "_" + req.body.company,
-        req.body.hire_date,
-        req.body.job,
-        req.body.salary,
-        req.body.dept_id,
-        req.body.mng_id,
-      ),
+    const employee = await updateDepartment(
+      req.body.company,
+      req.body.emp_id,
+      req.body.emp_name,
+      req.body.emp_no,
+      req.body.hire_date,
+      req.body.job,
+      req.body.salary,
+      req.body.dept_id,
+      req.body.mng_id,
     );
     res.status(200).send({ success: employee });
   } catch (error) {
