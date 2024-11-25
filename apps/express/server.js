795dcd4f4c5ddffe4b823c1a8bd9ef8c52c756eb
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-25 18:40:20 -0500

    insert tc bz

--- apps/express/server.js
+++ apps/express/server.js
@@ -11,6 +11,7 @@ import {
   getTimecard,
   insertDepartment,
   insertEmployee,
+  insertTimecard,
   updateDepartment,
 } from "./business-layer.js";
 import DataLayer from "./companydata/index.js";
@@ -198,72 +199,10 @@ app.get(root + "/timecard", async function (req, res) {
 
 app.post(root + "/timecard", async function (req, res) {
   try {
-    // company must be your RIT id
-    if (req.body.company !== "ctg7866") {
-      throw new Error("Company name must be your RIT username");
-    }
-
-    // emp_id must exist as the record id of an Employee in your company.
-    if (!(await dl.getEmployee(req.body.emp_id))) {
-      throw new Error("Employee not found");
-    }
-
-    // start_time must be a valid date and time equal to the current date
-    if (new Date(req.body.start_time).getDate() !== new Date().getDate()) {
-      // or back to the Monday prior to the current date if the current date is not a Monday.
-      if (new Date(req.body.start_time).getDay() !== 1) {
-        throw new Error("Start time must be a Monday");
-      } else {
-        throw new Error("Start time must be today");
-      }
-    }
-
-    // end_time must be a valid date and time at least 1 hour greater than the start_time and be on the same day as the start_time.
-    if (
-      new Date(req.body.end_time).getTime() -
-        new Date(req.body.start_time).getTime() <
-      3600000 // 1 hour in milliseconds
-    ) {
-      throw new Error("End time must be at least 1 hour after start time");
-    }
-
-    // start_time and end_time must be a Monday, Tuesday, Wednesday, Thursday or a Friday. They cannot be Saturday or Sunday.
-    if (
-      new Date(req.body.start_time).getDay() === 0 ||
-      new Date(req.body.start_time).getDay() === 6 ||
-      new Date(req.body.end_time).getDay() === 0 ||
-      new Date(req.body.end_time).getDay() === 6
-    ) {
-      throw new Error("Start and end time must be weekdays");
-    }
-
-    // start_time and end_time must be between the hours (in 24 hour format) of 08:00:00 and 18:00:00 inclusive.
-    if (
-      new Date(req.body.start_time).getHours() < 8 ||
-      new Date(req.body.start_time).getHours() >= 18 ||
-      new Date(req.body.end_time).getHours() < 8 ||
-      new Date(req.body.end_time).getHours() >= 18
-    ) {
-      throw new Error("Start and end time must be between 08:00 and 18:00");
-    }
-
-    // start_time must not be on the same day as any other start_time for that employee.
-    const timecards = await dl.getAllTimecard(req.body.emp_id);
-    for (const timecard of timecards) {
-      if (
-        new Date(req.body.start_time).getDate() ===
-        new Date(timecard.start_time).getDate()
-      ) {
-        throw new Error("Employee already clocked in today");
-      }
-    }
-
-    const timecard = await dl.insertTimecard(
-      new dl.timecard(
-        new Date(req.body.start_time),
-        new Date(req.body.end_time),
-        req.body.emp_id,
-      ),
+    const timecard = await insertTimecard(
+      req.body.start_time,
+      req.body.end_time,
+      req.body.emp_id,
     );
     res.status(200).send({
       success: timecard,
