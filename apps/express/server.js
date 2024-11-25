0f2f7ea1e96f7f46aa0e4521fa247a60b0e369b2
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-25 15:40:43 -0500

    insert emp

--- apps/express/server.js
+++ apps/express/server.js
@@ -58,6 +58,7 @@ app.post("/department", async function (req, res) {
 
 app.put("/department", async function (req, res) {
   try {
+    // dept_id must be an existing record number for a department
     if (!dl.getDepartment(req.body.company, req.body.dept_id)) {
       throw new Error("Department not found");
     }
@@ -108,6 +109,56 @@ app.get("/employee", async function (req, res) {
   try {
     const employee = await dl.getEmployee(req.query.company, req.query.emp_id);
     res.send(200).send(employee);
+  } catch (error) {
+    res.status(400).send({
+      error: error.message,
+    });
+  }
+});
+
+app.post("/exployee", async function (req, res) {
+  try {
+    // company – must be your RIT username
+    if (req.body.company !== "ctg7866") {
+      throw new Error("Company name must be your RIT username");
+    }
+
+    // dept_id must exist as a Department in your company
+    if (!dl.getDepartment(req.body.company, req.body.dept_id)) {
+      throw new Error("Department not found");
+    }
+
+    // mng_id must be the record id of an existing Employee in your company. Use 0 if the first employee or any other employee that doesn’t have a manager.
+    if (!dl.getEmployee(req.body.mng_id) && req.body.mng_id !== 0) {
+      throw new Error("Manager not found");
+    }
+
+    // hire_date must be a valid date equal to the current date or earlier (e.g. current date or in the past)
+    if (new Date(req.body.hireDate) > new Date()) {
+      throw new Error("Hire date must be in the past");
+    }
+
+    // hire_date must be a Monday, Tuesday, Wednesday, Thursday or a Friday. It cannot be Saturday or Sunday.
+    if (
+      new Date(req.body.hireDate).getDay() === 0 ||
+      new Date(req.body.hireDate).getDay() === 6
+    ) {
+      throw new Error("Hire date must be a weekday");
+    }
+
+    const employee = await dl.insertEmployee(
+      new dl.Employee(
+        req.body.emp_name,
+        // emp_no must be unique amongst all employees in the database, including those of other companies. You may wish to include your RIT user ID in the employee number somehow.
+        req.body.emp_no + "_" + req.body.company,
+        req.body.hire_date,
+        req.body.job,
+        req.body.salary,
+        req.body.dept_id,
+        req.body.mng_id,
+      ),
+    );
+    res.status(200).send({ success: employee });
   } catch (error) {
     res.status(400).send({
       error: error.message,
