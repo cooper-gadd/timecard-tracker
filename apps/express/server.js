7e3b88505ba57c9b3145736138d1a67191e0ce24
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-25 13:14:08 -0500

    depends

--- apps/express/server.js
+++ apps/express/server.js
@@ -1,24 +1,22 @@
-var express = require("express");
-var DataLayer = require("./companydata/index.js");
+import express, { json } from "express";
+import DataLayer from "./companydata/index.js";
 var dl = new DataLayer("ritusername");
 //now use dl.Department, dl.Employee and dl.TimeCard
-var logger = require('morgan');
+import logger from "morgan";
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
+app.use(logger("dev"));
+app.use(json());
 
 //use router if you'd like
 
 //use appropriate routes/paths/verbs
-app.get("/",async function(req,res){
-    //call the appropriate dl methods/objects using
-    //await as the data layer methods are asynchronous
-    res.json({"response":"this is the appropriate response"});  
+app.get("/", async function (req, res) {
+  //call the appropriate dl methods/objects using
+  //await as the data layer methods are asynchronous
+  res.json({ response: "this is the appropriate response" });
 });
 
-
 app.listen(8282);
-console.log('Express started on port 8282');
-
+console.log("API is running on http://localhost:8282");
